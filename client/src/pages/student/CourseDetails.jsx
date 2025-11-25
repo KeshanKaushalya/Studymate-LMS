b581b1d89a49ffd3d3784629e2fcfe5d367ce9ff
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-11-25 14:04:45 +0530

    Update CourseDetails.jsx

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
 
 const CourseDetails = () => {
+
+   const {id} = useParams()
+
+   const [courseData, setCoursedata] = useState(null)
+
+   const {allCourses} = useContext(AppContext)
+
+   const fetchCourseData = async ()=>{
+     const findCourse = allCourses.find(course => course._id === id)
+   }
+
   return (
     <div>
       <h1>Course Details Page</h1>
