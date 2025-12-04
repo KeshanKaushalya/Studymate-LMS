7bc1df7190d0152f66ab00958841a957f41e7734
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-04 16:41:26 +0530

    update

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // Connect to the MongoDB database
 
 const connectDB = async () =>{
-    mongoose.connection.on('connected', ()=> console.log('Database Connect successfully'))
+    mongoose.connection.on('connected', ()=> console.log('Database Connected'))
     mongoose.connection.on('error', (err) => console.log('Database Connection Error:', err.message))
 
     await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
