b509f9737e11da00078a48b745ebcd898832e172
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-09 12:01:45 +0530

    Update webhooks.js

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -23,7 +23,7 @@ export const clerkWebhooks = async (req, res) => {
                 const userData = {
                     _id: data.id,
                     email: data.email_addresses[0].email_address,
-                    name: data.fist_name + " " + data.last_name,
+                    name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
                 await User.create(userData)
@@ -34,7 +34,7 @@ export const clerkWebhooks = async (req, res) => {
             case 'user.updated': {
                 const userData = {
                     email: data.email_addresses[0].email_address,
-                    name: data.fist_name + " " + data.last_name,
+                    name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
                 await User.findByIdAndUpdate(data.id, userData)
